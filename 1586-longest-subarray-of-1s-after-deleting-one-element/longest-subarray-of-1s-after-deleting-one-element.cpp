fea206cff27cc66a8746279dc197799d2a8f7903
Sripramod Yogarajan <sripramodyogarajan1211@gmail.com>
2025-11-12 00:56:15 +0530

    Time: 0 ms (100.00%) | Memory: 60.1 MB (50.10%) - LeetSync

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        // Two-pointer Approach: Use a counter to ensure only one 1 has encountered so far !!
+        // If more than 1 one is encountered then move the left pointer until the count
+        // becomes 0 again, move the right pointer constantly 
+        int n = nums.size();
+        int left = 0, count = 0;
+        int maxLen = 0;
+        for(int right = 0;right < n;right ++){
+            if(nums[right] == 0) count ++;
+            while(count > 1) {
+                if(nums[left ++] == 0) count --;
+            }
+            maxLen = max(maxLen, right - left);
+        }
+        return maxLen;
+    }
+};
