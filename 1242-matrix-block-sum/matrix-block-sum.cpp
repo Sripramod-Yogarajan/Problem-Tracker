f522ec288b6a446dc1ce16c83604407effc97b74
Sripramod Yogarajan <sripramodyogarajan1211@gmail.com>
2025-11-16 12:24:22 +0530

    Time: 542 ms (6.28%) | Memory: 13.4 MB (98.32%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.cpp
+++ 1242-matrix-block-sum/matrix-block-sum.cpp
@@ -3,6 +3,7 @@ public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
         int m = mat.size(); // Number of rows
         int n = mat[0].size(); // Number of columns
+        // Brute-force approach: Simulate according to the question 
         vector<vector<int>> res(m, vector<int>(n, 0));
         for(int i = 0;i < m;i ++){
             for(int j = 0;j < n;j ++){
