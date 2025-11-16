1bd3d6a43f022b5f49964f23650f3b3f887db743
Sripramod Yogarajan <sripramodyogarajan1211@gmail.com>
2025-11-16 12:23:25 +0530

    Time: 549 ms (5.34%) | Memory: 13.4 MB (98.32%) - LeetSync

---
+++ 1242-matrix-block-sum/matrix-block-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
+        int m = mat.size(); // Number of rows
+        int n = mat[0].size(); // Number of columns
+        vector<vector<int>> res(m, vector<int>(n, 0));
+        for(int i = 0;i < m;i ++){
+            for(int j = 0;j < n;j ++){
+                for(int r = max(i - k, 0);r <= min(i + k, m-1);r ++){
+                    for(int c = max(j - k, 0); c <= min(j + k, n-1);c ++){
+                        res[i][j] += mat[r][c];
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
