ba6a8a4bb9f91a29169dbad062bc37aaa477f9a1
Sripramod Yogarajan <sripramodyogarajan1211@gmail.com>
2025-11-19 01:03:56 +0530

    Time: 37 ms (38.17%) | Memory: 61.6 MB (46.33%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        // Keyword: Kth largest -> Heaps
+        // Another approach is to use Divide and Conquer method !! (DAA)
+        // By default priority queues are max-heap 
+        // Syntax: prioriry_queue<datatype, container, comparing function> name (minheap)
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int num: nums){
+            pq.push(num);
+            if(pq.size() > k) pq.pop();
+        }
+        return pq.top();
+    }
+};
