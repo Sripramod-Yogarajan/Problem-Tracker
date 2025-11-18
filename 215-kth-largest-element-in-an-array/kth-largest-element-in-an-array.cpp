633739d437af36703ba54a6545e10038f275093b
Sripramod Yogarajan <sripramodyogarajan1211@gmail.com>
2025-11-19 01:05:51 +0530

    Time: 43 ms (19.17%) | Memory: 61.4 MB (61.92%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -5,15 +5,16 @@ public:
         // Another approach is to use Divide and Conquer method !! (DAA)
         // By default priority queues are max-heap 
         // Syntax: prioriry_queue<datatype, container, comparing function> name (minheap)
-        /* priority_queue<int, vector<int>, greater<int>> pq;
+        priority_queue<int, vector<int>, greater<int>> pq;
         for(int num: nums){
             pq.push(num);
             if(pq.size() > k) pq.pop();
         }
         return pq.top();
-        */
+        /* Another approach: Most optimal using built-in method in C++
         int n = nums.size();
-        nth_element(begin(nums), begin(nums) + n - k, end(nums));
+        nth_element(begin(nums), begin(nums)+n-k, end(nums));
         return nums[n-k];
+        */
     }
 };
