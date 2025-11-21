ab938fe78d25a4468d78bec99e3027c695b822c0
Sripramod Yogarajan <sripramodyogarajan1211@gmail.com>
2025-11-21 17:28:11 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (57.65%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        // Brute-force: Using two loops inside a loop O(n^2) 
+        // First loop to calculate the product of elements 0 to i-1
+        // Second loop to calculate the product of elements i+1 to n
+        // Optimal Approach: Using prefix and suffix arrays 
+        // prefix[i] = prefix[i-1] * nums[i-1] and suffix[i] = suffix[i+1] * nums[i+1]  
+        // Then res[i] = prefix[i] * suffix[i]
+        // Time and space: O(n)
+        // Follow Up: To solve it in O(1) space -> instead of using arrays we use 
+        // prefix and suffix variables    
         int n = nums.size();
-        vector<int> prefix(n), suffix(n);
-        prefix[0] = 1;
-        for(int i = 1;i < n;i ++){
-            prefix[i] = prefix[i-1] * nums[i-1];
-            
-        }
-
-        suffix[n-1] = 1;
-        for(int i = n-2;i >= 0;i --){
-            suffix[i] = suffix[i+1] * nums[i+1];
-        }
-
-        vector<int> res(n);
+        vector<int> res(n,1);
+        int prefix = 1;
         for(int i = 0;i < n;i ++){
-            res[i] = prefix[i] * suffix[i];
+            res[i] *= prefix;
+            prefix *= nums[i];
         }
-
+        int suffix = 1;
+        for(int i = n-1;i >= 0;i --){
+            res[i] *= suffix;
+            suffix *= nums[i];
+        }
         return res;
     }
 };
