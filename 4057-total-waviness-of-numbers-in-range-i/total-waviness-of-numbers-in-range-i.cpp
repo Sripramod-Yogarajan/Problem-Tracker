917f55312a9873926b839f62eba6761cdab3e39f
Sripramod Yogarajan <sripramodyogarajan1211@gmail.com>
2025-11-22 22:30:00 +0530

    Time: 53 ms (64.29%) | Memory: 10.1 MB (64.29%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
@@ -1,35 +1,24 @@
 class Solution {
 private:
-    int checkValley(vector<int>& nums){
+    int countWaviness(int n){
         int count = 0;
-        for(int i = 1;i < nums.size()-1;i ++){
-            if(nums[i] < nums[i-1] && nums[i] < nums[i+1]) count ++;
-        }
-        return count;
-    }
-
-    int checkPeak(vector<int>& nums){
-        int count = 0;
-        for(int i = 1;i < nums.size()-1;i ++){
-            if(nums[i] > nums[i-1] && nums[i] > nums[i+1]) count ++;
+        string s = to_string(n);
+        if(s.size() < 3) return 0;
+        for(int i = 1;i < s.size()-1;i ++){
+            int x = s[i-1] - '0';
+            int y = s[i] - '0';
+            int z = s[i+1] - '0';
+            if(y < x && y < z) count ++;
+            if(y > x && y > z) count ++;
         }
         return count;
     }
 public:
     int totalWaviness(int num1, int num2) {
-        if(num1 < 100 && num2 < 100) return 0;
-        if(num1 < 100) num1 = 100;
-        int sum = 0;
-        for(int i = num1; i <= num2;i ++){
-            vector<int> digits;
-            int num = i;
-            while(num > 0){
-                digits.push_back(num%10);
-                num /= 10;
-            }
-            reverse(digits.begin(), digits.end());
-            sum += checkPeak(digits) + checkValley(digits);
+        int total = 0;
+        for(int i = num1;i <= num2;i ++){
+            total += countWaviness(i);
         }
-        return sum;
+        return total;
     }
 };
