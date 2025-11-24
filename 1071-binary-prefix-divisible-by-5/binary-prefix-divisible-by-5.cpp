44291b615a8d2fa60ce83d236f4438867a403978
Sripramod Yogarajan <sripramodyogarajan1211@gmail.com>
2025-11-24 07:11:18 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (94.87%) - LeetSync

--- 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.cpp
+++ 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.cpp
@@ -4,6 +4,9 @@ public:
         vector<bool> res;
         int rem = 0;
         for(int b: nums){
+            // Binary concatenation: old_num * 2 + 1 or 0
+            // Eg: 11 (0101 (shift left)) -> 1010 (10) -> add 1 (current bit) -> 1011
+            // Multiply by 2 -> shift left !!!
             rem = ((rem << 1) + b) % 5;
             res.push_back(rem == 0);
         }
